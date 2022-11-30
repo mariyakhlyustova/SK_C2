7c8de30688f99b75f21f4632df1038e815bcb9b3
Mariya Khlyustova <7047373@mail.ru>
2022-12-01 02:11:35 +0300

    F Max array

---
+++ Ex009/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 5, 7, 8, 12, 7, 9, 56, 24 };
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
