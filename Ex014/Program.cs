8af98c4b245fd072b9fe946dc04d98fe099e18ef
Mariya Khlyustova <7047373@mail.ru>
2022-12-09 06:07:58 +0300

    работа метода с текстом

--- Ex014/Program.cs
+++ Ex014/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4 вид работа с текстом
+// поменять знаки одни на другие 
+
+string text = "в лесу родилась елочка, в лесу она росла";
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText, 'к', 'К');
+Console.WriteLine(newText);
