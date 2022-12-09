0d891c6d6395773ddab59d52003814fa73e023fe
Mariya Khlyustova <7047373@mail.ru>
2022-12-09 06:31:28 +0300

    сортировка массива

--- Ex015/Program.cs
+++ Ex015/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// сортировка массива методом
+
+int[] arr = {3, 5, 6, 8, 1, 6, 2, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j]< array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
