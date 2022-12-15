625ec8e3d2e4209f169ed8ce37d466bfc1a4e13b
Mariya Khlyustova <7047373@mail.ru>
2022-12-15 07:33:54 +0300

    слово

---
+++ Ex017/Program.cs
@@ -0,0 +1,11 @@
+﻿string[,] table = new string[2,5];
+
+table[1,2] = "слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int colums = 0; colums < 5; colums++)
+    {
+        Console.WriteLine($"-|{table[rows, colums]}|-");
+    }
+}
